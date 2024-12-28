3cb6a08ce31f78d842f5b6a9aaff4502fb014221
veraberger <60077645+veraberger@users.noreply.github.com>
2024-12-28 19:00:02 +0000

    Deploying to gh-pages from @ veraberger/veraberger.github.io@16b8ff63f1d812cbe52ff70205d0ed7f798b5c88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,6 +391,11 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',

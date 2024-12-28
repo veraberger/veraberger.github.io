bbc51b6ce540411e8ec9f38777745f36bffa9c2d
veraberger <60077645+veraberger@users.noreply.github.com>
2024-12-28 19:01:28 +0000

    Deploying to gh-pages from @ veraberger/veraberger.github.io@6baa01fce655aa75b8779bbab50d87f1577d229a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -381,9 +381,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-supernova-host-galaxies",
+          title: 'Supernova Host Galaxies',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",

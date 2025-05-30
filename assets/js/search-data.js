f8afafd871236815a976c8df069e340566758560
veraberger <60077645+veraberger@users.noreply.github.com>
2025-05-30 02:18:25 +0000

    Deploying to gh-pages from @ veraberger/veraberger.github.io@5e510aabeb0c437cd13162c1e4142e13810b0027 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,6 +389,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-curse-of-the-bambino",
+          title: 'curse of the bambino',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
